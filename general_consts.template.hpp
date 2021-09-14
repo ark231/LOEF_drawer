f3b9d052adf45bdff241864103d98d23fd4c2885
ark231 <arklp20909@gmail.com>
2021-09-14 16:40:48 +0900

    add: ソフト名、バージョン表示

---
+++ general_consts.template.hpp
@@ -0,0 +1,6 @@
+namespace LOEF {
+constexpr const char* application_name = "LOEF_drawer";
+constexpr const int version_major = ${LOEF_drawer_VERSION_MAJOR};
+constexpr const int version_minor = ${LOEF_drawer_VERSION_MINOR};
+constexpr const int version_patch = ${LOEF_drawer_VERSION_PATCH};
+}  // namespace LOEF
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,18 @@
 #include "mainwindow.hpp"
+
+#include <QString>
+
 #include "./ui_mainwindow.h"
+#include "general_consts.hpp"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    ui->label_app_info->setText(QStringLiteral("%1 \nv%2.%3.%4")
+                                    .arg(LOEF::application_name)
+                                    .arg(LOEF::version_major)
+                                    .arg(LOEF::version_minor)
+                                    .arg(LOEF::version_patch));
+    this->setWindowTitle(LOEF::application_name);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
-
+MainWindow::~MainWindow() { delete ui; }
