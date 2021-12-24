7af45da5ea33c292676f13d8186635ec78f14c49
ark231 <arklp20909@gmail.com>
2021-12-24 12:03:23 +0900

    change: バージョン情報等の埋め込みを別ファイルで
    定数ヘッダに変更を加えても、ビルドしてファイルが生成されるまではリンターからその変更が見えず、濡れ衣を着せられてしまうので、分離。
    ついでに、埋め込む前の目印の文字列はc++としては正しくないので、エラーになってしまうため、未生成のときはダミーの定義をするようにちょっとしたマクロを仕込んだ
    
    これと、Android向けのCMakeLists.txtの記述を削除した変更をマージ

--- libs/header/general_consts.hpp
+++ libs/header/general_consts.hpp
@@ -1,11 +1,9 @@
 #ifndef LOEF_DRAWER_GENERAL_CONSTS
 #define LOEF_DRAWER_GENERAL_CONSTS
+#include "buildtime_info.hpp"
 #include "units.hpp"
 namespace LOEF {
 constexpr const char* application_name = "LOEF_drawer";
-constexpr const int version_major = ${LOEF_drawer_VERSION_MAJOR};
-constexpr const int version_minor = ${LOEF_drawer_VERSION_MINOR};
-constexpr const int version_patch = ${LOEF_drawer_VERSION_PATCH};
 using id_type = size_t;
 
 /*GLOBAL VARIABLES!*/  // This is not the best solution,I know, but I don't have enough time
@@ -18,5 +16,6 @@ inline millimetre_quantity initial_fixed_pos_y = 50 * millimetre;
 inline coulomb_quantity initial_fixed_charge = 0.0 * boostunits::coulomb;
 inline inverse_permittivity_quantity initial_inverse_permittivity = 12.0 * inverse_permittivity_unit_quantity;
 inline millimetre_quantity interval_steps = 0.1 * millimetre;
+
 }  // namespace LOEF
 #endif
