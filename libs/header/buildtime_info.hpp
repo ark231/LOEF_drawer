51800586a9dff8ff131afa9bd3d0ad8757e394c4
ark231 <arklp20909@gmail.com>
2022-01-30 16:23:39 +0900

    change: clang-formatにかけた

--- libs/header/buildtime_info.hpp
+++ libs/header/buildtime_info.hpp
@@ -1,7 +1,7 @@
 #ifndef LOEF_DRAWER_BUILDTIME_INFO
 #define LOEF_DRAWER_BUILDTIME_INFO
 #if __has_include("buildtime_info_impl.hpp")
-#include "buildtime_info_impl.hpp"
+#    include "buildtime_info_impl.hpp"
 #else
 namespace LOEF {
 constexpr const int version_major = -1;
--- libs/header/buildtime_info_impl.template.hpp
+++ libs/header/buildtime_info_impl.template.hpp
@@ -1,8 +1,8 @@
 #ifndef LOEF_DRAWER_BUILDTIME_INFO_IMPL
 #define LOEF_DRAWER_BUILDTIME_INFO_IMPL
-namespace LOEF{
+namespace LOEF {
 constexpr const int version_major = ${LOEF_drawer_VERSION_MAJOR};
 constexpr const int version_minor = ${LOEF_drawer_VERSION_MINOR};
 constexpr const int version_patch = ${LOEF_drawer_VERSION_PATCH};
-}
+}  // namespace LOEF
 #endif
